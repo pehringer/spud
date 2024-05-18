f2ee127dac361bb834ebed05883d26bc40a76962
pehringer <pehringer.jacob@gmail.com>
2024-05-18 17:34:39 -0600

    example programs

---
+++ v2/example_asm/hello_world.asm
@@ -0,0 +1,21 @@
+GET get STR
+    set PUTC
+    get GET
+    add INC
+    set GET
+    any GET
+
+INC num 1
+
+STR num 104
+    num 101
+    num 108
+    num 108
+    num 111
+    num 32
+    num 119
+    num 111
+    num 114
+    num 108
+    num 100
+    num 10
