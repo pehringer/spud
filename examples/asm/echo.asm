d79a2183660adb3b62de1918507bbbb04b00e900
pehringer <pehringer.jacob@gmail.com>
2024-06-26 22:04:06 -0600

    good enough

--- examples/asm/echo.asm
+++ examples/asm/echo.asm
@@ -1,10 +1,17 @@
-LOOP get  ZERO
-     set  GETC
-     get  GETC
-     set  PUTC
-     get  CR
-     sub  GETC
-     neg  LOOP
-HALT any  HALT
+PROMPT get GT
+       set PUTC
+       set PUTC
+       get SP
+       set PUTC
+PRINTC get  ZERO
+       set  GETC
+       get  GETC
+       set  PUTC
+       get  CR
+       sub  GETC
+       neg  PRINTC
+       any  PROMPT
 ZERO 0
 CR 10
+SP 32
+GT 62
